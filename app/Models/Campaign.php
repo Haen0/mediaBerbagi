35d483926ee32ce8300d6d71e22e6a176b11af5c
Farhan <shukanechan@gmail.com>
2024-07-18 14:43:17 +0700

    BE : add crud article, article category, banner, home front, campaign, campaign category, urutkan kategori, urutkan campaign,

--- app/Models/Campaign.php
+++ app/Models/Campaign.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
@@ -14,9 +15,15 @@ class Campaign extends Model
         'title', 'slug', 'category', 'photo', 'content', 'target',
         'deadline', 'donate_button_label', 'operational_fund_percentage',
         'fundraiser_reward_percentage', 'nominal1', 'nominal2', 'nominal3',
-        'nominal4', 'nominal5', 'nominal6'
+        'nominal4', 'nominal5', 'nominal6', 'tampilkan', 'urutan'
     ];
 
+    protected static function booted()
+    {
+        static::creating(function ($campaign) {
+            $campaign->urutan = static::count() + 1;
+        });
+    }
 
     protected static function boot()
     {
@@ -43,5 +50,20 @@ class Campaign extends Model
         return is_null($value) ? 'unlimited' : $value;
     }
 
+    public function setDeadlineAttribute($value)
+    {
+        if ($value === 'unlimited') {
+            $this->attributes['deadline'] = null;
+        } else {
+            $this->attributes['deadline'] = $value;
+            // $this->attributes['deadline'] = Carbon::parse($value)->format('Y-m-d');
+        }
+
+    }
+
+    public function getDeadlineAttribute($value)
+    {
+        return is_null($value) ? 'unlimited' : $value;
+    }
 
 }
--- resources/views/admin/konten/webUtama/editSlider.blade.php
+++ resources/views/admin/konten/webUtama/editSlider.blade.php
@@ -1,25 +1,35 @@
 @extends('admin.layout.main')
 @section('konten')
-    <div class="p-4">
+    <div class="p-4 ">
         <div class="subheader text-white my-8">
             <div class="welcome  bg-green-700 me-30 w-full py-6 px-4 rounded-xl">
                 <div class="kanan my-auto">
-                    <a type="button" href="{{ route('articleCategory.index') }}"
+                    <a type="button" href="{{ route('banner.index') }}"
                         class="text-green-700 bg-white p-10 border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-semibold rounded-lg text-lg px-px py-2.5 w-32 h-12 text-center dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
                         Kembali
                     </a>
                 </div>
             </div>
-            <form method="POST" action="{{ route('articleCategory.store') }}" class="px-8 mt-6 bg-white rounded-lg">
+            <form method="POST" action="{{ route('banner.update', $banner->id) }}" enctype="multipart/form-data" class="px-8 mt-6 bg-white rounded-lg">
                 @csrf
+                @method('PUT')
                 <div class=" pt-4">
-                    <label for="kelola_kategori" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Kelola Kategori</label>
-                    <input type="text" id="kelola_kategori"
+                    <label for="link_target" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Link Target</label>
+                    <input name="link" type="text" id="link_target"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="Kelola Kategori" name="name" required />
+                        placeholder="Link Target" value="{{ $banner->link }}" required />
                 </div>
-
+                <div class=" pt-4">
+                    <label class="block mb-2 text-sm font-medium text-black dark:text-white"
+                        for="icon">Icon
+                        <p class="text-yellow-500">slider image recommendation 600x300px</p>
+                    </label>
+                    <input
+                        name="image" class="block w-full text-sm text-gray-500 border border-gray-300 rounded-md cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+                        id="icon" type="file" value="{{ $banner->image }}">
+                </div>
                 <div class="pt-4 flex justify-end">
+
                     <button type="submit"
                         class="text-white bg-green-700 border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-semibold rounded-lg text-xl pt-1.5 w-24 h-11 text-center dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
                         Simpan
